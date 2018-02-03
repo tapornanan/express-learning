93e9f4668b5aa545547036ba73690bf5403c4381
Pornanan Nanthapornpisut <tapornanan23@gmail.com>
2018-02-03 18:43:19 +0700

    Init git

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const hbs = require('hbs')
+const fs = require('fs')
+
+let app = express();
+
+// Register handlebar partials function.
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Helper function called in partial
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+// Helper function to capitalize text
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+})
+
+// set view engine to handlebar
+app.set('view engine', 'hbs');
+
+
+// next is called when finished.
+app.use((req, res, next) => {
+    // like check authentication if it succes call next();
+    let now = new Date().toString();
+    const log = `Incoming request Time: ${now}, Method: ${req.method}, Path: ${req.path}\n`;
+    console.log(log);
+    
+    fs.appendFile('server.log', log, (err) => {
+        if (err) console.log(err)
+        next();
+    })
+})
+
+app.use((req, res, next) => {
+    res.render('maintainance.hbs');
+})
+
+// middleware serve as static cotents.
+app.use(express.static(__dirname + '/public'))
+
+
+
+app.get('/', (req, res) => {
+    
+    res.render('home.hbs', {
+        pageTitle: 'Home',
+        welcomeMessage: 'YoLo Captain'
+    })
+});
+
+
+app.get('/about', (req, res) => {
+    console.log(req.params)
+    res.render('about.hbs', {
+        name: 'pornanan',
+        pageTitle: 'About'
+    });
+})
+
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Bad request!'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('server is running at port 3000...\nhttp://localhost:3000')
+});
